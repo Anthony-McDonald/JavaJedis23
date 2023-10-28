625d1ffa111911bc030d5339c706c6a1f61b0d3d
Wccchen <149198631+Wccchen@users.noreply.github.com>
2023-10-28 20:48:16 +0100

    updated tutorial & credit

--- PolarExStress/StartMenu.gd
+++ PolarExStress/StartMenu.gd
@@ -7,7 +7,16 @@ func _on_StartButton_pressed():
 	get_tree().change_scene("res://MainScene.tscn")
 
 func _on_TutorialButton_pressed():
-	pass # Replace with function body.
+	get_tree().change_scene("res://Tutorial.tscn")
 
 func _on_QuitButton_pressed():
 	get_tree().quit()
+
+
+func _on_BackButton_pressed():
+	get_tree().change_scene("res://StartMenu.tscn")
+
+	
+
+func _on_CreditButton_pressed():
+	get_tree().change_scene("res://Credit.tscn")
